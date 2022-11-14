fd1832186ce4a01645b2578b2d1af18cd1d3a050
Vinay Yadav <vinayyadav91190@gmail.com>
2022-11-14 18:17:07 +0530

    create a new class CTime.js to get current time

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Heading from './Components/heading.js';
 import Para from './Components/para.js';
 import List from './Components/list.js';
 import * as Cal from './Components/calculator.jsx';
+import Cards from './Components/Card.js';
 import './App.css';
 import './index.js';
 
@@ -15,6 +16,10 @@ const App = () => {
       <li>{Cal.Addition(2, 4)}</li>
       <li>{Cal.Multiplication(2, 4)}</li>
       <li>{Cal.Division(2, 4)}</li>
+      <h1 className="headingStyle">List of Top 5 Netflix Series</h1>
+      <Cards
+        imagesrc={Sdata[0].img.scr}
+      />
     </>
   );
 }
