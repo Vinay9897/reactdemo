45017ec227bdc51a350e3dbdce4c16136757ac01
Vinay Yadav <vinayyadav91190@gmail.com>
2023-02-07 22:55:42 +0530

    define the props and types

---
+++ src/practice/proptype.js
@@ -0,0 +1,51 @@
+import Proptype from 'prop-types';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+
+// Create a component with properties defined for
+// it and validate these props using PropTypes
+
+function Comp(props) {
+    return (
+        <>
+            <div className="card w-25">
+                <div className="card-body">
+                    <h5 className="card-title">{props.productName} - {props.colors} </h5>
+
+                    <p className="card-text">Price:
+                        {props.price}
+                    </p>
+                    {props.availability ? null : <p className="text-danger">Product out of stock</p>}
+                    <p>Rating: {props.feedback.rating}</p>
+                    <button onClick={props.addToCart} className="btn btn-primary">Add to Cart</button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+Comp.propType = {
+    productName: Proptype.string.isRequired,
+    price: Proptype.number,
+    colors: Proptype.colors,
+    availability: Proptype.bool,
+    feedback: Proptype.object,
+    addToCart: Proptype.func,
+};
+
+App.defaultProps = {
+    productName: "iPhone",
+    price: "36000",
+    colors: ["Black"],
+    availability: true,
+    feedback: {
+        rating: 4,
+        comments: "good",
+    },
+    addToCart: function (e) {
+
+    }
+};
+
+export default Comp;
+
